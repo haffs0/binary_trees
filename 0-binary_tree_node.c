8f6ce4c4ba84deb3919656aebc5d4cb58a79e2af
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-11-16 16:04:24 +0000

    update

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-	if (new == NULL)
+	if (new_node == NULL)
 		return (NULL);
 	new_node->n = value;
 	new_node->parent = parent;
