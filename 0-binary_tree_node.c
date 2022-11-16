fd61488c11162d85d3eed07d9fc23bbc20b4db30
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-11-16 16:02:45 +0000

    return null if memory is empty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,6 +11,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = new_node->right = NULL;
